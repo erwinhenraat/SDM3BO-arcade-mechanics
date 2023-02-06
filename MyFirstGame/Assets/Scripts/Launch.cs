5744a1b803598e36523c3e98d0cfeefa8d440879
Erwin Henraat <e.henraat@ma-web.nl>
2023-02-06 15:08:49 +0100

    done

--- MyFirstGame/Assets/Scripts/Launch.cs
+++ MyFirstGame/Assets/Scripts/Launch.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Launch : MonoBehaviour
 {
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("space")) {
+            Debug.Log("yo!");
+            rb.AddForce(transform.up * 300f);
+        }
     }
 }
